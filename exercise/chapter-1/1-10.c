5413c887f4f2d3c1ba2cf25c8b2a6572b624bb49
tatwd <leoking9641@hotmail.com>
2018-03-17 14:04:24 +0800

    add `1-10`

---
+++ exercise/chapter-1/1-10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+// #include <conio.h>
+
+int main () 
+{
+    int c;
+
+    while ((c = getchar()) != EOF) // getch()
+    {
+        if (c == '\t') {
+            // putchar('\\');
+            // putchar('t');
+            printf("\\t");
+        } else if (c == '\b') {
+            printf("\\b");
+        } else if (c == '\\') {
+            printf("\\\\");
+        } else {
+            putchar(c);
+        }
+    }
+    
+    return 0;
+}
