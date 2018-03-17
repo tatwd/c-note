c0ddb98eb89efff89de2bc54e63ab11d087851e5
tatwd <leoking9641@hotmail.com>
2018-03-17 19:44:13 +0800

    add `1-13` and example dir

---
+++ exercise/chapter-1/1-13.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#define IN  1   // 单词内
+#define OUT 0   // 单词外
+#define N   50  // 数组的长度
+
+int main () 
+{
+    int c, nword, state, i;
+    int wlen[N];
+    
+    // init
+    c = nword = 0;
+    state = OUT;
+    for (i = 0; i < N; ++ i)
+        wlen[i] = 0;
+
+    // get length
+    while ((c = getchar()) != EOF) {
+        if (c < 'A' || (c > 'Z' && c < 'a') || c > 'z')
+           state = OUT;
+        else if (state == OUT) {
+            state = IN;
+            ++ nword;
+        } 
+        
+        if (state == IN)
+            ++ wlen[nword - 1];
+    }
+
+    // print
+    printf("nword: %d\nword length:\n", nword);
+    for (i = 0; i < nword; ++ i) 
+        printf("%d ", wlen[i]);
+
+    printf("\n");
+    for (i = 0; i < nword; ++ i) 
+        printf("%d\n", wlen[i]);
+
+    return 0;
+}
