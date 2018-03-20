8707c08d592d5e3a38dcbc4b2490d2365b5b7b98
tatwd <leoking9641@hotmail.com>
2018-03-20 19:47:09 +0800

    add `1-18`

---
+++ exercise/chapter-1/1-18.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define N 100
+
+int main()
+{
+    int c, i, n, nline;
+    char s[N];
+    
+    n = nline = 0;
+    for (i = 0; i < N - 1 && (c = getchar()) != EOF; ++i) {
+        if (c == '\t' || c == ' ') {
+            ++n;
+            continue;
+        }
+        if (c == '\n')
+            ++nline;
+        s[i - n] = c;
+    }
+    s[i - n] = '\0';
+    printf("nline: %d\nresult:\n%s", nline, s);
+
+    return 0;
+}
