21ca6e3b615474259637814f6b4ed4e77179797c
tatwd <leoking9641@hotmail.com>
2018-03-21 10:42:41 +0800

    add `1-20`

--- exercise/chapter-1/1-20.c
+++ exercise/chapter-1/1-20.c
@@ -3,13 +3,35 @@
 #define MAX 100
 #define N 5 /* 每隔 5 列出现一次制表符终止位 */
 
+int get_line(char s[], int max);
 void copy(char to[], char from[]);
 void detab(char s[]);
 
 int main()
 {
+    int c, i;
+    char s[MAX];
+
+    while (get_line(s, MAX) > 0) {
+        detab(s);
+        printf("=>%s", s);
+    }
 
     return 0;
+}
+
+int get_line(char s[], int max)
+{
+    int c, i;
+
+    for (i = 0; i < max - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
 }
 
 void copy(char to[], char from[])
@@ -20,28 +42,26 @@ void copy(char to[], char from[])
 }
 
 /*
- * translate tab with `n` space
+ * translate tab with some space
  */
 void detab(char s[])
 {
-    int i, pos, n;
+    int i, j, pos, n;
     char cp[MAX];
 
     copy(cp, s);
+
     pos = n = 0;
-    for (i = 0; i < MAX - 1 && cp[i] != '\0', ++i) {
-        pos = pos % N;
-        
-        if (cp[i] == '\t' && i != 0) {
-            while (( n = (N - pos)) > 0) {
-                i = i + n - 1;
-                s[i] = ' ';
-                n--;
-            }
-            pos = 0;
+    for (i = j = 0; i < MAX - 1 && cp[i] != '\0'; ++i) {
+        pos = i % N;
+        if (cp[i] == '\t' && pos != 0) {
+            n = N - pos;
+            while (j - i < n)
+                s[j++] = '*'; /* add space */
         } else {
-            s[i] = cp[i];
-            ++pos;
+            s[j] = cp[i];
+            ++j;
         }
     }
+    s[j] = '\0';
 }
