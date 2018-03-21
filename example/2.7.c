b3355781d4674f1d5c99f1b670c799f03788851b
tatwd <leoking9641@hotmail.com>
2018-03-21 15:32:06 +0800

    add ex.2.7

---
+++ example/2.7.c
@@ -0,0 +1,25 @@
+/* 
+ * atoi 函数：将字符串 s 转化为对应的整型数 
+ * e.g. "12345" => 12345
+ */
+int atoi(char s[])
+{
+    int i, n;
+
+    n = 0;
+    for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i)
+        n = 10 * n + (s[i] - '0');
+    return n;
+}
+
+/*
+ * lower 函数：将字符 c 转化成小写形式；只对 ASCII 字符集有效
+ * e.g. 'A' => 'a'
+ */
+int lower(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+        return c + 'a' - 'A';  /* or c + 32 */
+    else 
+        return c;
+}
