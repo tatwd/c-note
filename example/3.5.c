8b2aec543d853298825835393bdac5582c56dc71
tatwd <leoking9641@hotmail.com>
2018-03-25 11:55:34 +0800

    add

---
+++ example/3.5.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+
+int atoi(char s[])
+{
+    int i, n, sign;
+
+    for (i = 0; isspace(s[i]); i++)  /* 跳过空白符 */
+        ;
+    sign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-')  /* 跳过空白符 */
+        i++;
+    for (n = 0; isdigit(s[i]); i++)
+        n = 10 * n + (s[i] - '0');
+    return sign * n;
+}
