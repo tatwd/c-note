88427c3d757b46454b4ee44dd10c096df9af9db0
tatwd <leoking9641@hotmail.com>
2018-03-25 13:31:51 +0800

    fix

--- exercise/chapter-3/3-01.c
+++ exercise/chapter-3/3-01.c
@@ -5,7 +5,7 @@ int binsearch(int x, int v[], int n);
 int main()
 {
     int index;
-    int v[6] = {1, 2, 3, 4, 5, 6};
+    int v[10] = { 1, 2, 3, 4, 5, 6 };
 
     index = binsearch(3, v, 6);
     printf("%d\n", index);
@@ -20,18 +20,20 @@ int binsearch(int x, int v[], int n)
 {
     int low, high, mid;
 
-    mid = (n - 1) / 2;
-    low = x > v[mid] ? mid + 1 : 0;
-    high = x < v[mid] ? mid -1 : n - 1;
+    low = 0;
+    high = n - 1;
+    mid = (low + high) / 2;
 
     /* 循环内部只执行了一次测试 */
-    while (low < high) {
+    while (low < high && x != v[mid]) {
         if (x < v[mid])
             high = mid - 1;
-        else if (x > v[mid])
-            low = mid + 1;
         else
-            return mid;  /* 找到匹配项 */
+            low = mid + 1;
+        mid = (low + high) / 2;
     }
-    return -1;  /* 未找到匹配项 */
+    if (x == v[mid])
+        return mid;  /* 找到匹配项 */
+    else
+        return -1;  /* 未找到匹配项 */
 }
