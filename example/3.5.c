b0faafc40236c2b1502fd85a6b163cb4dd541cb2
tatwd <leoking9641@hotmail.com>
2018-03-25 12:26:50 +0800

    add `shellsort`

--- example/3.5.c
+++ example/3.5.c
@@ -13,3 +13,19 @@ int atoi(char s[])
         n = 10 * n + (s[i] - '0');
     return sign * n;
 }
+
+/*
+ * 希尔排序：按递增循序对 v[0] ... v[n - 1] 进行排序
+ */
+void shellsort(int v[], int n)
+{
+    int gap, i, j, temp;
+
+    for (gap = n/2; gap > 0; gap /= 2)
+        for (i = gap; i < n; i++)
+            for (j = i-gap; j > 0 && v[j] > v[j+gap]; j -= gap) {
+                temp = v[j];
+                v[j] = v[j+gap];
+                v[j+gap] = temp;
+            }
+}
