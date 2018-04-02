ab59a895ce1f6f4a845b9bcd53836fca0ad2bbe7
tatwd <leoking9641@hotmail.com>
2018-04-02 09:09:54 +0800

    add solution 2 - O(n)

--- exercise/chapter-4/4-01.c
+++ exercise/chapter-4/4-01.c
@@ -14,19 +14,37 @@ int main()
     return 0;
 }
 
-/*
+/**
  * get right index that t in s
  */
 int strrindex(char s[], char t[])
 {
-    int i, j, k;
+    /*
+     * O(N^2)
+     */
+    /* int i, j, k;
 
-    for (i = 0; s[i] != '\0'; i++) {
+    for (i = 0, j = 0; s[i] != '\0'; i++) {
         for (j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++)
             ;
         if (k >= 0 && t[k] == '\0')
-            return i + k - 1;
+        return i + k - 1;
     }
+    return -1; */
 
+    /*
+     * O(N)
+     */
+    int i, j;
+
+    for (i = 0, j = 0; s[i] != '\0'; i++) {
+        if (s[i] == t[j])
+            j++;
+        else
+            j = 0;
+
+        if (j >= 0 && t[j] == '\0')
+            return i;
+    }
     return -1;
 }
